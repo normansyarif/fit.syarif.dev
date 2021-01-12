242893c473cacb696aee20d8aa6e6795fa5e2956
Norman Syarif <norman@syarif.dev>
2021-01-13 01:03:36 +0700

    update

--- glowup.php
+++ glowup.php
@@ -3,8 +3,8 @@
 <!DOCTYPE html>
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>2022 Glow-up Program</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>2022 Glow-up Program</title>
 
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
@@ -24,59 +24,59 @@
 
 <div class="container">
     <div class="row">
-		<div class="col-12 col-md-6 offset-md-3 col-lg-6 offset-lg-3 wrapper">
-		    
-		    <h4>2022 Glow-up Program</h4>
-	        
+        <div class="col-12 col-md-6 offset-md-3 col-lg-6 offset-lg-3 wrapper">
+            
+            <h4>2022 Glow-up Program</h4>
+            
             <p class="label">Goals: </p>
 
             <ol>
-            	<li>Body weight of 65kg or less</li>
-            	<li>Fewer acne scars</li>
-            	<li>Brighter skin color</li>
-            	<li>Smell good</li>
-            	<li>Own at least 5 new t-shirts and 1 pair of new shoes</li>
-            	<li>Own a watch</li>
-            	<li>Be hygienic</li>
+                <li>Body weight of 65kg or less</li>
+                <li>Fewer acne scars</li>
+                <li>Brighter skin color</li>
+                <li>Smell good</li>
+                <li>Own at least 5 new t-shirts and 1 pair of new shoes</li>
+                <li>Own a watch</li>
+                <li>Be hygienic</li>
             </ol>
             
             <p class="label">How to:</p>
             
             <ol>
-            	<li>Run/walk every day at 8am for at least 30 min</li>
-            	<li>Do workout and lift everyday at 5:30pm</li>
-            	<li>Eat no more than 1,000 kcal daily
-            		<ul>
-            			<li>Breakfast hours at 9am to 12pm</li>
-            			<li>Lunch hours at 12pm to 3pm</li>
-            			<li>Drink only water</li>
-            			<li>Only one cheat day is allowed per week</li>
-            			<li>Do the intense diet on cheat day</li>
-            		</ul>
-            	</li>
-            	<li>Fail to meet the daily real target requires you to do the intense diet on the next day</li>
-            	<li>Apply elixskin after fajr, dhuhr, and maghrib</li>
-            	<li>Apply naturgo before bed</li>
-            	<li>Save 250k a month for
-            		<ul>
-            			<li>T-shirt</li>
-            			<li>Shoes</li>
-            			<li>Naturgo</li>
-            			<li>Elixskin</li>
-            			<li>Perfume</li>
-            		</ul>
-            	</li>
-            	<li>Keep your bedroom clean</li>
-            	<li>Don't wear dirty clothes</li>
-            	<li>Wash the helmet, bed cover, and pillow cover every 2 weeks</li>
-            	<li>Take shower at least once a day, after workout</li>
-            	<li>Wash your face and brush your teeth twice a day, morning and before bed</li>
+                <li>Run/walk every day at 8am for at least 30 min</li>
+                <li>Do workout and lift everyday at 5:30pm</li>
+                <li>Eat no more than 1,000 kcal daily
+                    <ul>
+                        <li>Breakfast hours at 9am to 12pm</li>
+                        <li>Lunch hours at 12pm to 3pm</li>
+                        <li>Drink only water</li>
+                        <li>Only one cheat day is allowed per week (< 1,500 kcal)</li>
+                        <li>Run 10 minutes more on cheat day</li>
+                    </ul>
+                </li>
+                <li>Fail to meet the daily real target requires you to do the intense diet on the next day</li>
+                <li>Apply elixskin after fajr, dhuhr, and maghrib</li>
+                <li>Apply naturgo before bed</li>
+                <li>Save 250k a month for
+                    <ul>
+                        <li>T-shirt</li>
+                        <li>Shoes</li>
+                        <li>Naturgo</li>
+                        <li>Elixskin</li>
+                        <li>Perfume</li>
+                    </ul>
+                </li>
+                <li>Keep your bedroom clean</li>
+                <li>Don't wear dirty clothes</li>
+                <li>Wash the helmet, bed cover, and pillow cover every 2 weeks</li>
+                <li>Take shower at least once a day, after workout</li>
+                <li>Wash your face and brush your teeth twice a day, morning and before bed</li>
             </ol>
             
             <p class="label">Intense diet:</p>
             <ol>
-            	<li>Calories limit on this day is 600 kcal</li>
-            	<li>Run/walk 10 min more</li>
+                <li>Calories limit on this day is 600 kcal</li>
+                <li>Run/walk 10 min more</li>
             </ol>
             
             <div style="margin-bottom: 50px"></div>
